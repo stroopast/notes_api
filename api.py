6990689ca29c7014eea031ee67abdb485a838b9b
nanur <nanuroberto20@gmail.com>
2025-02-22 19:17:07 +0200

    added the database, added get method to retrive all notes and post method to add a note to db

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
+api = Api(app)
 
 class NoteModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,6 +16,35 @@ class NoteModel(db.Model):
     def __repr__(self):
         return f"Note(name = {self.title}, content = {self.content}, created_at = {self.created_at})"
 
+note_args = reqparse.RequestParser()
+note_args.add_argument('title', type=str, required=True, help="Title cannot be blank")
+note_args.add_argument('content', type=str, required=True, help="Content cannot be blank")
+note_args.add_argument('created_at', type=str, required=False)
+
+noteFields = {
+    'id':fields.Integer,
+    'title':fields.String,
+    'content':fields.String,
+    'created_at':fields.DateTime,
+}
+
+class Notes(Resource):
+    @marshal_with(noteFields)
+    def get(self):
+        notes = NoteModel.query.all()
+        return notes
+    
+    @marshal_with(noteFields)
+    def post(self):
+        args = note_args.parse_args()
+        note = NoteModel(title=args["title"], content=args["content"], created_at=args["created_at"])
+        db.session.add(note)
+        db.session.commit()
+        notes = NoteModel.query.all()
+        return notes, 201
+    
+
+api.add_resource(Notes, '/api/notes/')
 
 @app.route('/')
 def home():
