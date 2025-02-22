88ef65a3749d0a7a9dd8bd127ee78a30b62e4a88
nanur <nanuroberto20@gmail.com>
2025-02-22 18:51:23 +0200

    created note model database using sqlalchemy

--- api.py
+++ api.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
+class NoteModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f"Note(name = {self.title}, content = {self.content}, created_at = {self.created_at})"
+
+
 @app.route('/')
 def home():
     return '<h1> Flask REST API </h1>'
